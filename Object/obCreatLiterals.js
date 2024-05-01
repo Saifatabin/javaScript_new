1cb1953890ca7e01c99a5e85bdb29c6be7d06fda
Sk Mahammad Saif Atabin Yasar <153271513+Saifatabin@users.noreply.github.com>
2024-05-01 17:38:11 +0000

    object details

--- Object/obCreatLiterals.js
+++ Object/obCreatLiterals.js
@@ -4,10 +4,13 @@ const Juser={
     location:"Odisha",
     email:"s#***#@gmail.com",
     isLogedIn:false,
+    greeting:function(){console.log("hello js user")},
     lastLogInDays:["monday","saturday"],
     }
-console.log(Juser);
 console.log(Juser.email);
 console.log(Juser["age"]);
 Juser.location="balasore"
+Object.freeze(Juser)
+Juser.location="odisha"
 console.log(Juser);
+console.log(Juser.greeting());
