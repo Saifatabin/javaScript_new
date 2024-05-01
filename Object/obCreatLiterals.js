fc289f15cea47d5002ab6985bf6370f251d85028
Sk Mahammad Saif Atabin Yasar <153271513+Saifatabin@users.noreply.github.com>
2024-05-01 03:48:23 +0000

    object details

---
+++ Object/obCreatLiterals.js
@@ -0,0 +1,9 @@
+const Juser={
+    name:"saif",
+    age:30,
+    location:"Odisha",
+    email:"s#***#@gmail.com",
+    isLogedIn:false,
+    lastLogInDays:["monday","saturday"],
+    }
+console.log(Juser);
