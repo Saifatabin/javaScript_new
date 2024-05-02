62a0ac63cebf22e3050537296468781978d95b00
Sk Mahammad Saif Atabin Yasar <153271513+Saifatabin@users.noreply.github.com>
2024-05-02 14:01:09 +0000

    object details

--- Object/mergeObject.js
+++ Object/mergeObject.js
@@ -2,3 +2,7 @@ const object1={a:1,b:2}
 const object2={c:3,d:4}
 const object3=Object.assign({},object1,object2);
 console.log(object3);
+const ob1={a:"apple",b:"bat"}
+const ob2={c:"catch",d:"dot"}
+const ob3={...ob1,...ob2}
+console.log(ob3)
