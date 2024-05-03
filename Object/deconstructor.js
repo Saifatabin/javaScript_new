310a2fdd138d8689d1d1201820661a25999fcdb9
Sk Mahammad Saif Atabin Yasar <153271513+Saifatabin@users.noreply.github.com>
2024-05-03 06:04:43 +0000

    object details

--- Object/deconstructor.js
+++ Object/deconstructor.js
@@ -6,3 +6,6 @@ courseInstructor:"saif"
 }
 const {courseInstructor}=course;//by deconstructing you can access object like this but ,if you dont use deconstructing then you have to write "course.courseInstructor" every time you want to access it
 console.log(courseInstructor)
+//other way of deconstructing
+const {courseInstructor:instructor}=course
+console.log(instructor)
