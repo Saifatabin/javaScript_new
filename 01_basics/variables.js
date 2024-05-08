2b2354af85d645f17d243effe1858e9b71674200
Sk Mahammad Saif Atabin Yasar <153271513+Saifatabin@users.noreply.github.com>
2024-05-08 05:07:07 +0000

    variable details

--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -2,3 +2,12 @@ const a=12;//cannot be changed but mejorly prefered this then "let"
 let b=13;// you can change the variable 
 /*const a=14;//here is the example*/
 console.table(a,b);
+/*
+number:-2^53
+bigInt:-longer then numb only used in stock like or facebook like product
+string:-""
+boolean:-true/false
+null:-stand alone value
+undefine:-declared but not assign
+symbol:-unique
+*/
